9b1fd70a2f32b3531c2f8e0d3f11d09199b304ad
CiccioTecchio <fr.vicidomini94@gmail.com>
2022-04-13 00:16:53 +0200

    Body -refs #1 draft of home page

--- random-question/src/components/body/Body.tsx
+++ random-question/src/components/body/Body.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { ReactElement, useState } from 'react';
 import {IconProp} from '@fortawesome/fontawesome-svg-core';
-import {faFileUpload} from '@fortawesome/free-solid-svg-icons';
+import {faFileUpload, faPlay} from '@fortawesome/free-solid-svg-icons';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {Container, Row, Col, Button} from 'react-bootstrap';
+import {Container, Row, Col, Button, InputGroup, Form} from 'react-bootstrap';
 import {useTranslation} from 'react-i18next';
 import './Body.scss';
 
-function uploadBtn(uploadStr:string):any {
+// eslint-disable-next-line no-unused-vars
+enum StatusValidation {
+  // eslint-disable-next-line no-unused-vars
+  Valid = 'is-valid',
+  // eslint-disable-next-line no-unused-vars
+  Invalid = 'is-invalid',
+  // eslint-disable-next-line no-unused-vars
+  Empty = ''
+}
+
+function uploadBtn():ReactElement {
+  const [t] = useTranslation();
   return (
     <Button>
       <Row>
@@ -14,46 +25,76 @@ function uploadBtn(uploadStr:string):any {
           <FontAwesomeIcon icon={faFileUpload as IconProp} size="lg"/>
         </Col>
         <Col md={10}>
-          <label>{uploadStr}</label>
+          <label>{t('upload.json')}</label>
         </Col>
       </Row>
     </Button>
   );
 }
 
-function setTimerBtn(timerStr:string):any {
+function setTimerBtn():ReactElement {
+  const [t] = useTranslation();
   return (
-    <Button>{timerStr}</Button>
+    <Button>{t('set.timer')}</Button>
   );
 }
 
-function uploadBtnAndTimer(uploadStr:string, timerStr:string):any {
+function validateInputTimer(value:string):StatusValidation {
+  let classToAppend: StatusValidation = StatusValidation.Empty;
+  const regExp = new RegExp(/^[1-9]\d{0,1}$/);
+  if (value === '') classToAppend = StatusValidation.Empty;
+  else {
+    if (regExp.test(value)) classToAppend = StatusValidation.Valid;
+    else classToAppend = StatusValidation.Invalid;
+  }
+  return classToAppend;
+}
+
+function passingScoreInput():ReactElement {
+  const [passingScore, setPassingScore] = useState('');
+  const [t] = useTranslation();
   return (
-    <Row className="mt-3 text-center">
-      <Col md={6}>
-        {uploadBtn(uploadStr)}
-      </Col>
-      <Col md={6}>
-        {setTimerBtn(timerStr)}
-      </Col>
+    <Form>
+      <Form.Group controlId='passingScoreControl'>
+        <InputGroup>
+          <input className={`form-control ${validateInputTimer(passingScore)}`} type={'text'} inputMode='decimal'
+            placeholder={t('label.passing.score')} min={1} max={999} pattern="^[1-9]\d{0,1}$" onInput={(event)=>{
+              event.currentTarget.value = event.currentTarget.value.replace(/\D/, '');
+              setPassingScore(event.currentTarget.value);
+            } } />
+          <InputGroup.Text>%</InputGroup.Text>
+        </InputGroup>
+      </Form.Group>
+    </Form>
+  );
+}
+
+function uploadBtnPassingScoreAndTimer():ReactElement {
+  return (
+    <Row className="mt-5 text-center">
+      <Col> {uploadBtn()} </Col>
+      <Col> {passingScoreInput()} </Col>
+      <Col> {setTimerBtn()} </Col>
     </Row>
   );
 }
 
 export default function Body() {
   const {t} = useTranslation();
-
   return (
-    <Container className="body-cmp">
+    <Container>
       <Row className="mt-5 text-center">
         <h1>{t('title')}</h1>
       </Row>
       <Row className="mt-3 text-center">
         <h3>{t('explain')}</h3>
       </Row>
-      {uploadBtnAndTimer(t('upload.json'), t('set.timer'))}
+      {uploadBtnPassingScoreAndTimer()}
+      <div className='mt-5 text-center'>
+        <Button className='play-btn' size='lg' disabled>
+          <FontAwesomeIcon icon={faPlay as IconProp}/>
+        </Button>
+      </div>
     </Container>
   );
 }
-
-
