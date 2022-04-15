6c3bc6fd76021183851254e75947df49ea999fa2
CiccioTecchio <fr.vicidomini94@gmail.com>
2022-04-15 23:37:17 +0200

    body -refs #1 home page added upload button logic & ModalWrongFile

--- random-question/src/components/body/Body.tsx
+++ random-question/src/components/body/Body.tsx
@@ -1,8 +1,8 @@
-import React, { ReactElement, useState } from 'react';
+import React, { ReactElement, useRef, useState } from 'react';
 import {IconProp} from '@fortawesome/fontawesome-svg-core';
-import {faFileUpload, faPlay} from '@fortawesome/free-solid-svg-icons';
+import {faFileArrowUp, faPlay} from '@fortawesome/free-solid-svg-icons';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {Container, Row, Col, Button, InputGroup, Form} from 'react-bootstrap';
+import {Container, Row, Col, Button, InputGroup, Form, Alert, Modal} from 'react-bootstrap';
 import {useTranslation} from 'react-i18next';
 import './Body.scss';
 
@@ -22,19 +22,81 @@ interface ICanStart {
   isValidTimer?: boolean;
 }
 
+interface IUploadedFile{
+  name: string;
+  contentFile: any;
+  showAlert: boolean;
+}
+
+function ModalWrongFile(props:{showModal:boolean, onChangeShow:Function}):ReactElement {
+  const {t} = useTranslation();
+  const [show, setShow] = useState(props.showModal);
+
+  const handleClose = () => {
+    setShow(false);
+    props.onChangeShow(show);
+  };
+
+  const handleShow = () => {
+    setShow(true);
+    props.onChangeShow(show);
+  };
+
+  return (
+    <Modal show={props.showModal} onHide={handleShow}>
+      <Modal.Header>
+        <Modal.Title>{t('error.wrong.format')}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Alert variant='danger'>
+          <span dangerouslySetInnerHTML={{__html: t('error.wrong.format.desciption')}}></span>
+        </Alert>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant='secondary' onClick={handleClose}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
 function UploadBtn():ReactElement {
   const {t} = useTranslation();
+  const [uploadedFile, setUploadFile] = useState<IUploadedFile>({
+    name: '',
+    contentFile: {},
+    showAlert: false,
+  });
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleUpload = () => inputRef.current?.click();
+
+  const handleChangeShow = (change:boolean) => setUploadFile({name: uploadedFile.name, contentFile: uploadedFile.contentFile, showAlert: change});
+
+  const handleShowFile = () =>{
+    if (inputRef.current?.files && inputRef.current?.files[0].name.endsWith('.json')) {
+      const file:File = inputRef.current.files[0];
+      const nameFile = file.name;
+      const reader:FileReader = new FileReader();
+      reader.onload = (event) =>{
+        const res:any = event && event.target && event.target.result;
+        setUploadFile({name: nameFile, contentFile: JSON.parse(res), showAlert: false});
+      };
+      reader.readAsText(file);
+    } else {
+      setUploadFile({name: '', contentFile: {}, showAlert: true});
+    }
+  };
+
   return (
-    <Button>
-      <Row>
-        <Col md={2}>
-          <FontAwesomeIcon icon={faFileUpload as IconProp} size="lg"/>
-        </Col>
-        <Col md={10}>
-          <label>{t('upload.json')}</label>
-        </Col>
-      </Row>
-    </Button>
+    <div>
+      <label className='mx-3'>{t('upload.json')}</label>
+      <input ref={inputRef} onChange={handleShowFile} className='d-none' type='file' />
+      <Button variant={`outline-${uploadedFile.name?'success':'primary'}`} onClick={handleUpload}>
+        <FontAwesomeIcon className='mr-5' icon={faFileArrowUp as IconProp} />
+        <span>{uploadedFile.name ? uploadedFile.name : 'Upload'}</span>
+      </Button>
+      <ModalWrongFile showModal={uploadedFile.showAlert} onChangeShow = {handleChangeShow}/>
+
+    </div>
   );
 }
 
@@ -101,7 +163,7 @@ export default function Body():ReactElement {
         <h1>{t('title')}</h1>
       </Row>
       <Row className="mt-3 text-center">
-        <h3>{t('explain')}</h3>
+        <h3 dangerouslySetInnerHTML={{__html: t('explain')}}></h3>
       </Row>
       <UploadBtnPassingScoreAndTimer />
       <div className='mt-5 text-center'>
